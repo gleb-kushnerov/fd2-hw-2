150413ee7e6ccdd79424c1214401a657a0dc30e7
Che1sea57 <38821256+Che1sea57@users.noreply.github.com>
2019-03-19 22:58:35 +0300

    вторая версия, добавил функцию, создающую объект

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
 (function () {
     'use strict';
 
-    let tagField = {
-        element: document.createElement('div'),
-        getTags: function () {
-            return [...this.element.children].filter(element => element.classList.contains('tag'));
-        },
-        destroy: function () {
-            this.element.removeEventListener('keydown', createTag);
-            this.element.removeEventListener('click', removeTag);
-            this.element.remove();
-        }
-    },
-        tagFieldEl = tagField.element,
-        inputEl = document.createElement('input');
+    function createTagsField() {
+        let tagField = {
+                element: document.createElement('div'),
+                getTags: function () {
+                    return [...this.element.children].filter(element => element.classList.contains('tag'));
+                },
+                destroy: function () {
+                    this.element.removeEventListener('keydown', createTag);
+                    this.element.removeEventListener('click', removeTag);
+                    this.element.remove();
+                }
+            },
+            tagFieldEl = tagField.element,
+            inputEl = document.createElement('input');
 
-    document.body.append(tagFieldEl);
-    tagFieldEl.classList.add('tag-field');
-    tagFieldEl.append(inputEl);
-    tagFieldEl.addEventListener('keydown', createTag);
-    tagFieldEl.addEventListener('click', removeTag);
+        tagFieldEl.classList.add('tag-field');
+        tagFieldEl.append(inputEl);
+        tagFieldEl.addEventListener('keydown', createTag);
+        tagFieldEl.addEventListener('click', removeTag);
 
-    function createTag(event) {
-        if (event.key === 'Enter' || event.key === ',')  {
-            if (event.key === ',') {
-                event.preventDefault();
+        function createTag(event) {
+            if (event.key === 'Enter' || event.key === ',')  {
+                if (event.key === ',') {
+                    event.preventDefault();
+                }
+                let tagEl = document.createElement('div');
+                tagFieldEl.append(tagEl);
+                tagEl.classList.add('tag');
+                tagEl.textContent = `${inputEl.value}`;
+                inputEl.value = ``;
             }
-            let tagEl = document.createElement('div');
-            tagFieldEl.append(tagEl);
-            tagEl.classList.add('tag');
-            tagEl.textContent = `${inputEl.value}`;
-            inputEl.value = ``;
         }
+
+        function removeTag(event) {
+            let tag = event.target.closest('.tag');
+            if (tag)  {
+                tag.remove();
+            }
+        }
+
+        return tagField;
     }
 
-    function removeTag(event) {
-        let tag = event.target.closest('.tag');
-        if (tag)  {
-            tag.remove();
-        }
-    }
+    let tagField = createTagsField();
+    document.body.append(tagField.element);
+
 }());
